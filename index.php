14af8aa626aefe523a5942494689fe22099bbd27
László Patrik <laszlo.patrik@vereb.dc>
2019-11-05 17:55:29 +0100

    Grid

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
     <head>
         <meta http-equiv="content-type" content="text/html"; charset="UTF-8">
         <title>Git</title>
+        <style>@import 'style.css';</style>
     </head>
     <body>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div id="content">
+            <div id="A">A</div>
+            <div id="B">B</div>
+            <div id="C">C</div>
+            <div id="D">D</div>
+            <div id="E">E</div>
+            <div id="F">F</div>
+            <div id="G">G</div>
+            <div id="H">H</div>
+        </div>
     </body>
 </html>
